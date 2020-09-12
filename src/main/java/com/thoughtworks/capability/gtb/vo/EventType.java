77a976b283fc12f7534ce8709a85fbc7f9ad338a
zuowen.Wang <798389920@qq.com>
2020-09-12 18:05:14 +0800

    finish Problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventType.java
@@ -1,10 +1,15 @@
 package com.thoughtworks.capability.gtb.vo;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum EventType {
   UPLOAD("U"), DOWNLOAD("D");
 
   private String code;
-
+  @JsonValue
+  public String getString(){
+    return this.code;
+  }
   EventType(String code) {
     this.code = code;
   }
--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.NUMBER;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +20,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonFormat(shape = NUMBER)
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
